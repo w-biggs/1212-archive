fb055626eadc9c9de5aa02be710191dde73d47c5
w-biggs <wilson@wilsonbiggs.com>
2019-04-19 03:01:05 -0400

    Limit width of content

--- web/app/themes/onetwoonetwo/resources/views/partials/header.blade.php
+++ web/app/themes/onetwoonetwo/resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header class="banner">
+<header class="banner site-wrapper">
   <div class="container">
     <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     <nav class="nav-primary">
