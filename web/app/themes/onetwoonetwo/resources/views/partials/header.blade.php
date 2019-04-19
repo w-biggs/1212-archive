99f59af4ed81fa085e62e1e3be9cd7a6d982bb67
w-biggs <wilson@wilsonbiggs.com>
2019-04-19 03:27:26 -0400

    Basic initial styling

--- web/app/themes/onetwoonetwo/resources/views/partials/header.blade.php
+++ web/app/themes/onetwoonetwo/resources/views/partials/header.blade.php
@@ -1,6 +1,8 @@
 <header class="banner site-wrapper">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+    <a class="brand" href="{{ home_url('/') }}">
+      <img src="@asset('images/OneTwoOneTwo.svg')" alt="{{ get_bloginfo('name', 'display') }}">
+    </a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
