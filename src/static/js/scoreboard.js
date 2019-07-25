aa70e808f40da96eaa4a55465ce2065e017663b7
W Biggs <wilson@wilsonbiggs.com>
2019-07-25 14:33:20 -0400

    show two rows

--- src/static/js/scoreboard.js
+++ src/static/js/scoreboard.js
@@ -1,3 +1,47 @@
+const { debounce } = require('./vendor/underscore.min');
+
+const checkOverflow = function checkIfScoresAreOverflowingAndShowButton() {
+  const scoreboardGamesContainer = document.getElementsByClassName('scoreboard-games')[0];
+  const scoreboardGames = document.getElementsByClassName('scoreboard-game');
+  const gameCount = scoreboardGames.length;
+
+  const contWidth = scoreboardGamesContainer.getBoundingClientRect().width;
+
+  const gameStyle = getComputedStyle(scoreboardGames[0]);
+  const gameWidth = scoreboardGames[0].getBoundingClientRect().width
+    + parseInt(gameStyle.marginLeft, 10)
+    + parseInt(gameStyle.marginRight, 10);
+
+  if (gameCount > 10) { // More than 10 games
+    return true;
+  }
+  if (gameCount > 8 && (gameWidth * 5) > contWidth) { // overflowing two rows of 4
+    return true;
+  }
+  if (gameCount > 6 && (gameWidth * 4) > contWidth) { // overflowing two rows of 3
+    return true;
+  }
+  if (gameCount > 4 && (gameWidth * 3) > contWidth) { // overflowing two rows of 2
+    return true;
+  }
+  if (gameCount > 2 && (gameWidth * 2) > contWidth) { // overflowing two rows of 1
+    return true;
+  }
+
+  return false;
+};
+
+const checkShowButton = function checkIfExpandScoreboardButtonShouldShow() {
+  const overflow = checkOverflow();
+  const scoreboard = document.getElementsByClassName('scoreboard')[0];
+
+  if (overflow) {
+    scoreboard.classList.add('is-overflowing');
+  } else {
+    scoreboard.classList.remove('is-overflowing');
+  }
+};
+
 const reloadScores = function reloadScoresWithPostRequest() {
   const scoreboardContainer = document.getElementsByClassName('scoreboard-container')[0];
 
@@ -11,33 +55,64 @@ const reloadScores = function reloadScoresWithPostRequest() {
         placeholderDiv.firstChild,
         scoreboardContainer,
       );
+      checkShowButton();
     }
   };
   request.send();
 };
 
-reloadScores();
+const setMaxHeight = function setMaxHeightOfCollapsedScoreboard() {
+  const scoreboardGame = document.getElementsByClassName('scoreboard-game')[0];
 
-setInterval(reloadScores, 60000);
+  const gameHeight = scoreboardGame.getBoundingClientRect().height;
+  const gameStyle = getComputedStyle(scoreboardGame);
 
-let expandText = '';
+  const maxHeight = (gameHeight
+    + parseInt(gameStyle.marginTop, 10)
+    + parseInt(gameStyle.marginBottom, 10))
+    * 2;
 
-document.addEventListener('click', (event) => {
-  if (event.target.parentNode.classList.contains('scoreboard-expand')) {
-    const scoreboardGames = document.getElementsByClassName('scoreboard-games')[0];
-    const scoreboardExpand = scoreboardGames.getElementsByClassName('scoreboard-expand')[0];
-    const scoreboardExpandButton = scoreboardExpand.getElementsByTagName('button')[0];
-    expandText = expandText || scoreboardExpandButton.innerText;
-    const { collapseText } = scoreboardExpandButton.dataset;
+  const styleEl = document.createElement('style');
+  styleEl.appendChild(document.createTextNode(`.scoreboard.is-collapsed .scoreboard-games{ max-height: ${maxHeight.toFixed(2)}px }`));
 
-    if (scoreboardExpandButton.getAttribute('aria-expanded') === 'true') {
-      scoreboardGames.classList.add('is-collapsed');
-      scoreboardExpandButton.innerText = expandText;
-      scoreboardExpandButton.setAttribute('aria-expanded', 'false');
-    } else {
-      scoreboardGames.classList.remove('is-collapsed');
-      scoreboardExpandButton.innerText = collapseText;
-      scoreboardExpandButton.setAttribute('aria-expanded', 'true');
+  document.getElementsByTagName('head')[0].appendChild(styleEl);
+};
+
+const clickListener = function addScoreboardExpandButtonClickListener() {
+  let expandText = '';
+
+  document.addEventListener('click', (event) => {
+    if (event.target.parentNode.classList.contains('scoreboard-expand')) {
+      const scoreboard = document.getElementsByClassName('scoreboard')[0];
+      const scoreboardExpand = scoreboard.getElementsByClassName('scoreboard-expand')[0];
+      const scoreboardExpandButton = scoreboardExpand.getElementsByTagName('button')[0];
+      expandText = expandText || scoreboardExpandButton.innerText;
+      const { collapseText } = scoreboardExpandButton.dataset;
+  
+      if (scoreboardExpandButton.getAttribute('aria-expanded') === 'true') {
+        scoreboard.classList.add('is-collapsed');
+        scoreboardExpandButton.innerText = expandText;
+        scoreboardExpandButton.setAttribute('aria-expanded', 'false');
+      } else {
+        scoreboard.classList.remove('is-collapsed');
+        scoreboardExpandButton.innerText = collapseText;
+        scoreboardExpandButton.setAttribute('aria-expanded', 'true');
+      }
     }
-  }
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  reloadScores();
+  setInterval(reloadScores, 60000);
+
+  setMaxHeight();
+
+  checkShowButton();
+
+  clickListener();
+});
+
+window.addEventListener('resize', () => {
+  debounce(checkShowButton, 300);
 });
